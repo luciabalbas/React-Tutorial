4a2f7ba7dfea5ee3adfd0a70fc5ac22d7d703673
Lucia Balbas <luciabalrub@gmail.com>
2024-09-02 17:36:42 +0200

    Nesting

--- 01-Fundamentos/src/index.js
+++ 01-Fundamentos/src/index.js
@@ -1,25 +1,27 @@
-// Import to Root - Create Root Component
+// I M P O R T   T O   R O O T - Create Root Component - Not necessary bc React 17
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// First Component
-function Greeting() {
-  return (
-    <React.Fragment>
-      <div className="someValue">
-        <h2>My First Component</h2>
-        <ul>
-          <li>
-            <a href="">hello world</a>
-          </li>
-        </ul>
-      </div>
-      {/* // Only one parent item */}
-      <h3>hello world</h3>
-      <input type="text" name="" id="" />
-    </React.Fragment>
-  )
-}
+// F I R S T   C O M P O N E N T
+// function Greeting() {
+//   return (
+//     // <React.Fragment>
+//     <>
+//       <div className="someValue">
+//         <h2>My First Component</h2>
+//         <ul>
+//           <li>
+//             <a href="">hello world</a>
+//           </li>
+//         </ul>
+//       </div>
+//       {/* // Only one parent item */}
+//       <h3>hello world</h3>
+//       <input type="text" name="" id="" />
+//       </>
+//     // </React.Fragment>
+//   )
+// }
 // export default Greeting;
 
 // function Greeting() {
@@ -29,6 +31,23 @@ function Greeting() {
 //     React.createElement('h2', {}, 'hello world')
 //   )
 // }
+
+// N E S T   C O M P O N E N T
+function Greeting() {
+  return (
+    <div>
+      {/* <h2>John Doe</h2> */}
+      <Person />
+      {/* <p>This is my message</p> */}
+      <Message />
+    </div>
+  )
+}
+
+const Person = () => <h2>john doe</h2>
+const Message = () => {
+  return <p>this is my message</p>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
