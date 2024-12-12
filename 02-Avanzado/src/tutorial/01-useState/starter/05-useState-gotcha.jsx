a72a2b8ccf9807a5eb8f96872753ee9378df3cc1
Lucia Balbas <luciabalrub@gmail.com>
2024-12-12 13:32:45 +0100

    'Gotcha' using UseState Function

--- 02-Avanzado/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 02-Avanzado/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -3,9 +3,17 @@ import { useState } from 'react'
 const UseStateGotcha = () => {
   const [value, setValue] = useState(0)
   const handleButton = () => {
-    setValue(value + 1)
+    // el log va mal porque empieza en el 0, cualquier funcionalidad que dependa de él estará mal
+    // setValue(value + 1)
+    // console.log(value)
+
+    setValue((currentSate) => {
+      const newSate = currentSate + 1
+      return newSate
+    })
     console.log(value)
   }
+
   return (
     <>
       <h2>useState "gotcha"</h2>
