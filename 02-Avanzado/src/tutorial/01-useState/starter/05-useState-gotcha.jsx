0d6c18c485b6333a233680c58006c48470217f57
Lucia Balbas <luciabalrub@gmail.com>
2024-12-12 13:25:45 +0100

    Setup 'Gotcha' UseState

--- 02-Avanzado/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 02-Avanzado/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,21 @@
-const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
-};
+import { useState } from 'react'
 
-export default UseStateGotcha;
+const UseStateGotcha = () => {
+  const [value, setValue] = useState(0)
+  const handleButton = () => {
+    setValue(value + 1)
+    console.log(value)
+  }
+  return (
+    <>
+      <h2>useState "gotcha"</h2>
+      <h2>{value}</h2>
+      <button className="btn" onClick={handleButton}>
+        Aumenta
+      </button>
+      <hr></hr>
+    </>
+  )
+}
+
+export default UseStateGotcha
