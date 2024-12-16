f14731f82d7ce38f320c65d4baac0307aa0bfc69
Lucia Balbas <luciabalrub@gmail.com>
2024-12-16 12:55:00 +0100

    useEffect Basics

--- 02-Avanzado/src/tutorial/02-useEffect/starter/02-useEffect-basics.jsx
+++ 02-Avanzado/src/tutorial/02-useEffect/starter/02-useEffect-basics.jsx
@@ -1,20 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react'
 
 const UseEffectBasics = () => {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(0)
   const sayHello = () => {
-    console.log('hello there');
-  };
+    console.log('hello there')
+  }
 
-  sayHello();
+  sayHello()
+
+  useEffect(() => {
+    console.log('hello from useEffect')
+  }, [])
+  // las [] es donde depende el re-render, si vacío => solo al cargar la página
 
   return (
     <div>
       <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
+      <button className="btn" onClick={() => setValue(value + 1)}>
         click me
       </button>
     </div>
-  );
-};
-export default UseEffectBasics;
+  )
+}
+export default UseEffectBasics
