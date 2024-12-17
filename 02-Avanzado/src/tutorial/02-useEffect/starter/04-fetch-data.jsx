69ffc7f2ac471cb15174bba73c9f5ef9976e27c5
Lucia Balbas <luciabalrub@gmail.com>
2024-12-17 20:22:08 +0100

    Fetch Challenge

--- 02-Avanzado/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ 02-Avanzado/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,43 @@
-const url = 'https://api.github.com/users';
+import { useState, useEffect } from 'react'
+
+const url = 'https://api.github.com/users'
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
-};
-export default FetchData;
+  const [users, setUser] = useState([])
+
+  useEffect(() => {
+    console.log('holi')
+    const getUsers = async () => {
+      try {
+        const resp = await fetch(url)
+        const users = await resp.json()
+        // console.log(users)
+        setUser(users)
+        console.log(users)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getUsers()
+  }, [])
+  return (
+    <section>
+      <h2>fetch data example</h2>
+      <ul>
+        {users.map((user) => {
+          const { login, avatar_url, html_url, id } = user
+          return (
+            <li id={id}>
+              <h4>{login}</h4>
+              <img src={avatar_url} alt={login} width={100} />
+              <p>
+                <a href={html_url}>Github</a>
+              </p>
+            </li>
+          )
+        })}
+      </ul>
+    </section>
+  )
+}
+export default FetchData
